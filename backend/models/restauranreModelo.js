e1087a0764dfe8925f45c6563a977578b59a9b94
Melvin <melsava1207@gmail.com>
2024-08-05 18:54:16 -0600

    Modelo de restaurantes terminado

---
+++ backend/models/restauranreModelo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  nombre:{
+    type: String,
+    required: [true, "Ingresa el nombre del restaurante"]
+  },
+  latitud:{
+    type: Number,
+    required: [true, 'Ingresa la latitud del restaurante']
+  },
+  longitud:{
+    type: Number,
+    required: [true, 'Ingresa la longitud del restaurante']
+  },
+  horarios:{
+    type: String,
+    required: [true, 'Ingresa el horario del restaurante']
+  },
+  likes:{
+    type: Number,
+    required: [true, 'Indica si te gusto o no la pelicula'],
+    default: 0
+  },
+  dislikes:{
+    type: Number,
+    required: [true, 'Indica si te no gusto la pelicula'],
+    default: 0
+  },
+  rating:{
+    type: Number,
+    required: [true, 'Indica el promedio de las puntuaciones de la pelicula'],
+    default: 0
+  },
+  votos:{
+    type: Number,
+    required: [true, 'Indica la puntuación para la pelicula del 1 al 10'],
+    default: 0
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Restaurante', userSchema)
