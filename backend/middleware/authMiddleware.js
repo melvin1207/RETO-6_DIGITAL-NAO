c3db59bfc64a8d6f8046231b87e14393b81adffb
Melvin <melsava1207@gmail.com>
2024-08-09 07:09:26 -0600

    MIddleware comentado

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,30 +4,38 @@ const asyncHandler = require('express-async-handler')
 const Usuario = require('../models/usuarioModelos')
 
 
-//la función permite solo editar los objetos que se tienen si y solo si se cuenta con un token de acceso si no no se puede realizar un cambio
+/*la función permite solo editar los objetos que se tienen si y solo si se cuenta con un token de acceso 
+si no no se puede realizar un cambio */
 const protect = asyncHandler(async(req, res, next) => {
   let  token
   
+  //se verificara si se tiene un berare token de acceso
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
     try{
+
+      //si lo tiene se procede a obtener el usuario correspondiente
       token = req.headers.authorization.split(' ')[1]
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
       req.usuario = await Usuario.findById(decoded.id_usuario).select('-password')
       
+      //se pasa a lo siguiente en el codigo
       next()
     } catch(error){
+      //si no hay un usuario se lanza un error
       console.log(error)
       res.status(401)
       throw new Error('Acceso no permitido')
     }
   }
 
+  //si no hay un token se lanza un nuevo error
   if(!token){
     res.status(401)
     throw new Error('No hay un token disponible, acceso no autorizado')
   }
 })
 
+//se exporta la función
 module.exports = { protect }
--- backend/middleware/userMiddleware.js
+++ backend/middleware/userMiddleware.js
@@ -1,7 +1,10 @@
+//paquetes necesarios
 const asyncHandler = require('express-async-handler')
 
+//función para verificar si el usuario logueado es el correspondiente
 const userProtect = asyncHandler(async(req, res, next) =>{
   try{
+    // se comprueba si el id del usuario con el del loguado son iguales
     if(req.usuario.id !== req.params.id){
       res.status(401)
       throw new Error("Not authorized")
@@ -9,10 +12,13 @@ const userProtect = asyncHandler(async(req, res, next) =>{
       next()
     }
   } catch(error){
+
+    //si los id no son iguales se lanza un error
     console.log(error)
     res.status(401)
     throw new Error('Acceso no permitido')
   }
 })
 
+//se exporta la función
 module.exports = { userProtect }
